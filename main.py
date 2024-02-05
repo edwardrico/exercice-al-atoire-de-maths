770c0eaa4ef698f61025e2d47b72bfd84a472aad
edwardrico <120380332+edwardrico@users.noreply.github.com>
2024-02-05 18:25:13 +0100

    minuteur

--- main.py
+++ main.py
@@ -1,58 +1,65 @@
 import random
+import time
 
 NB_MIN = 0
-NB_MAX = 10
+NB_MAX = 5
 QUESTION = 4
+TEMPS_TOTAL = 30
 
 
 def exercise_aleatoire():
     a = random.randint(NB_MIN, NB_MAX)
     b = random.randint(NB_MIN, NB_MAX)
-    operateur = random.randint(1, 4)
+    operateur = random.randint(1, 2)
 
     if operateur == 1:
         operateur = "+"
-
     elif operateur == 2:
         operateur = "-"
-
     elif operateur == 3:
         operateur = "*"
-
     elif operateur == 4:
         operateur = "/"
 
     resultat = eval(f"{a}{operateur}{b}")
-    equation = float(input(f"Quelle est le resultat de : {a} {operateur} {b} = "))
+    equation = float(input(f"Quel est le résultat de : {a} {operateur} {b} = "))
     if equation == resultat:
         return True
     else:
-        print(f"La reponse correcter est : {resultat}")
+        print(f"La réponse correcte est : {resultat}")
         return False
 
 
 points = 0
+temps_debut = time.time()
+for i in range(1, QUESTION + 1):
+    temps_actuel = time.time()
+    temps_ecoule = temps_actuel - temps_debut
+    temps_restant = max(0, TEMPS_TOTAL - temps_ecoule)
 
-for i in range(0, QUESTION):
-    print(f"C'est partie exercise n° {i + 1}")
+    print(f"C'est parti, exercice n° {i}")
+    print(f"Temps restant : {int(temps_restant)} secondes")
+    if temps_ecoule >= TEMPS_TOTAL:
+        print("Le temps est écoulé, vous avez perdu!")
+        break
+    time.sleep(1)
     if exercise_aleatoire():
         points += 1
-        print(f"reponse correcte, vous cumule {points} point ")
-
+        print(f"Réponse correcte, vous cumulez {points} point(s)")
     else:
-        print(f"Reponse incorrecte ")
+        print("Réponse incorrecte")
     print()
 
-
-pourcentage = (points/QUESTION) * 100
-print(f"Vous avez une total de {points}/{QUESTION} points. Le que equibo a {round(pourcentage)} % de reusite")
+pourcentage = (points / QUESTION) * 100
+print(
+    f"Vous avez un total de {points}/{QUESTION} points. Le questionnaire a un taux de réussite de {round(pourcentage)} %")
 print()
 
 if pourcentage == 100:
-    print("Super vous est trop bon est maths !")
+    print("Super, vous êtes trop bon en maths !")
 elif pourcentage == 0:
-    print("Vous devrie revise vos maths car, vous est pas bon !")
+    print("Vous devriez réviser vos maths, car vous n'êtes pas bon !")
 elif pourcentage >= 50:
-    print("Pas mal vous est eu desus de la moyen!")
+    print("Pas mal, vous êtes au-dessus de la moyenne !")
 elif pourcentage <= 49:
-    print("Vous devrei prectique plus !")
+    print("Vous devriez pratiquer plus !")
